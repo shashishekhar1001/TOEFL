edc484b0456187bea6c4a04c0cdc0c9d4373c4f5
Shashishekhar Hasabnis <shashishekhar1001@gmail.com>
2018-04-17 09:43:37 +0530

    Managed Static Files

--- Vocabulary/admin.py
+++ Vocabulary/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from Vocabulary.models import *
 # Register your models here.
+
+class WordAdmin(admin.ModelAdmin):
+    list_display = ["id", "word_vocab", "meaning", "image_url"]
+    class Meta:
+        model = Word
+
+class Quiz_QuestionAdmin(admin.ModelAdmin):
+    list_display = ["quiz", "q_type", "text", "correct"]
+    class Meta:
+        model = Quiz_Question
+
+admin.site.register(Word, WordAdmin)
+admin.site.register(Answer_Options)
+admin.site.register(Quiz)
+admin.site.register(Quiz_Question, Quiz_QuestionAdmin)
--- Vocabulary/models.py
+++ Vocabulary/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
 # Create your models here.
+class Word(models.Model):
+    word_vocab = models.CharField(max_length=200)
+    meaning = models.CharField(max_length=300)
+    image_url = models.ImageField(upload_to='word_pics/')
+
+    def __str__(self):
+        return self.word_vocab
+
+    class Meta:
+        verbose_name = "Word"
+        verbose_name_plural = "Words"
+
+
+
+class Answer_Options(models.Model):
+    text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = "Answer Option"
+        verbose_name_plural = "Answer Options"
+
+
+
+class Quiz(models.Model):
+    quiz_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.quiz_name
+
+    class Meta:
+        verbose_name = "Quiz"
+        verbose_name_plural = "Quizes"
+
+
+class Quiz_Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, blank=True, default=None, null=True)
+    QUESTION_CHOICES = (
+        ('FILL_IN_THE_BLANK', 'FILL_IN_THE_BLANK'),
+        ('SELECT_BEST_MATCH', 'SELECT_BEST_MATCH'),
+    )
+    q_type = models.CharField(max_length=30, choices=QUESTION_CHOICES)
+    text = models.CharField(max_length=200)
+    possible_answers = models.ManyToManyField(Answer_Options)
+    selected = models.ForeignKey(Answer_Options, related_name="selected", default=None, on_delete=models.CASCADE, blank=True, null=True)
+    correct = models.ForeignKey(Answer_Options, related_name="correct", default=None, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = "Quiz Question"
+        verbose_name_plural = "Quiz Questions"
