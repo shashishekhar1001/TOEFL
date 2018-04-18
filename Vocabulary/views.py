324b7a2ee8c66ec044fedf57354506f8abe1f05b
Shashishekhar Hasabnis <shashishekhar1001@gmail.com>
2018-04-18 15:35:23 +0530

    AD

--- Vocabulary/views.py
+++ Vocabulary/views.py
@@ -1,5 +1,11 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
+from Vocabulary.models import *
 
 # Create your views here.
 def build_vocab(request):
-    return render(request, "build_vocab.html", {})
+    query_set = Word.objects.get_queryset().order_by('id')
+    paginator = Paginator(query_set, 1) # Show 3 words per page
+    page = request.GET.get('page')
+    words = paginator.get_page(page)
+    return render(request, "build_vocab.html", {"query_set":query_set, "words": words})
